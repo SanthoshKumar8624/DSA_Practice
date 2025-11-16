7a04aa08477b71a6f1fb56298f2cc5fa5abb490a
santhosh <santhoshkmr.sdr@gmail.com>
2025-11-16 18:32:45 +0530

    moves zero at last in the array

--- Practice/src/RemoveDup/MoveZeros.java
+++ Practice/src/RemoveDup/MoveZeros.java
@@ -1,4 +1,30 @@
 package RemoveDup;
 
-public class MoveZeros {
+import java.util.Arrays;
+
+public class MoveZeros
+{
+    public static int [] mz(int [] a)
+    {
+        int slow=0;
+        for(int fast=0;fast< a.length;fast++)
+        {
+            if(a[fast]!=0)
+            {
+                a[slow]=a[fast];
+                slow++;
+            }
+        }
+        for (int i=slow;i< a.length;i++)
+        {
+            a[i]=0;
+        }
+        return a;
+    }
+    public static void main(String[] args)
+    {
+       int [] arr={10,0,7,47,0};
+        System.out.println(" Moments before disaster : "+Arrays.toString(arr));
+        System.out.println("          After          : "+Arrays.toString(mz(arr)));
+    }
 }
