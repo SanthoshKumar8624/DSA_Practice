42eabb0d67f997203842cef49f9e3dd9d2d55e47
santhosh <santhoshkmr.sdr@gmail.com>
2025-12-24 11:46:42 +0530

    Ops with prime

---
+++ Practice/src/Prime/rangePrime.java
@@ -0,0 +1,35 @@
+package Prime;
+import java.util.Scanner;
+public class rangePrime
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int j=n+1;j<100;j++)
+        {
+            n=j;
+            boolean flag=true;
+            if(n<=1)
+            {
+                flag=false;
+            }
+            else
+            {
+                for(int i=2;i<=Math.sqrt(n);i++)
+                {
+                    if(n%i==0)
+                    {
+                        flag=false;
+                        break;
+                    }
+                }
+            }
+            if(flag)
+            {
+                System.out.println("Next Prime is :"+n);
+            }
+        }
+    }
+}
+
