aea82dc307bda2f1e891c3e4852add696fe914fc
santhosh <santhoshkmr.sdr@gmail.com>
2026-01-09 23:33:48 +0530

    Day5

---
+++ Practice/src/TwoPointers/TrappingRainWater.java
@@ -0,0 +1,49 @@
+package TwoPointers;
+
+import java.util.Arrays;
+
+public class TrappingRainWater
+{
+    public static int trapWater(int[] height)
+    {
+        int left=0;
+        int right=height.length-1;
+        int maxLeft=0;
+        int maxRight=0;
+        int water=0;
+        while(left<right)
+        {
+            if(height[left]<height[right])
+            {
+                if(height[left]>=maxLeft)
+                {
+                    maxLeft=height[left];
+                }
+                else
+                {
+                    water+=maxLeft-height[left];
+                }
+                left++;
+            }
+            else
+            {
+                if(height[right]>=maxRight)
+                {
+                    maxRight=height[right];
+                }
+                else
+                {
+                    water+=maxRight-height[right];
+                }
+                right--;
+            }
+        }
+        return water;
+    }
+    public static void main(String[] args)
+    {
+        int [] height={4,2,0,3,2,5};
+        System.out.println(Arrays.toString(height));
+        System.out.println(trapWater(height));
+    }
+}
