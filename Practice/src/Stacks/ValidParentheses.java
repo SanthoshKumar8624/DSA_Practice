9877b61dfded95300cb6b5bd44d9b9abc21b0a62
santhosh <santhoshkmr.sdr@gmail.com>
2026-01-30 12:09:04 +0530

    Checking Valid parentheses

---
+++ Practice/src/Stacks/ValidParentheses.java
@@ -0,0 +1,41 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class ValidParentheses
+{
+    public static boolean isValidParentheses(String str)
+    {
+        Stack<Character> stack=new Stack<>();
+        int n=str.length();
+        for(int i=0;i<n;i++)
+        {
+            char c=str.charAt(i);
+            if(c=='('||c=='{'||c=='[')
+            {
+                stack.push(c);
+            }
+            else
+            {
+                if(stack.isEmpty())
+                {
+                    return false;
+                }
+                else
+                {
+                    char top=stack.pop();
+                    if((c==')'&&top!='(')||(c=='}'&&top!='{')||(c==']'&&top!='['))
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        String str="{([])}";
+        System.out.println(str);
+        System.out.println(isValidParentheses(str));
+    }
+}
