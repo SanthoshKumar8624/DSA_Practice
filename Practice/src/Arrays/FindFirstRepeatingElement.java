37eb16556d34f88addf133a601a73c956569cf85
santhosh <santhoshkmr.sdr@gmail.com>
2026-02-08 13:49:09 +0530

    program to find FirstRepeatingElement

--- Practice/src/Arrays/FindFirstRepeatingElement.java
+++ Practice/src/Arrays/FindFirstRepeatingElement.java
@@ -1,9 +1,7 @@
 package Arrays;
-
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
-
 public class FindFirstRepeatingElement
 {
     public static void main(String[] args) {
