9ddcd4f4064d7b51b72aaa5ebe245fbec4f950c3
santhosh <santhoshkmr.sdr@gmail.com>
2026-03-17 12:10:57 +0530

    rotation changes

--- Practice/src/Arrays/leftRotate.java
+++ Practice/src/Arrays/leftRotate.java
@@ -4,19 +4,60 @@ import java.util.Arrays;
 
 public class leftRotate
 {
-    public static void lRotate(int[] a, int k)
+    public static void Reverse(int[] a, int start, int end)
     {
-        k = k % a.length;
-        for(int i=0;i<a.length;i++)
+        while(start<end)
         {
-            System.out.print(i+k<a.length? a[i+k]+" " : a[i+k- a.length]+" ");
+            int temp=a[start];
+            a[start]=a[end];
+            a[end]=temp;
+            start++;
+            end--;
         }
     }
     public static void main(String[] args)
     {
         int [] a={1,2,3,4,5};
-        int k=1;
         System.out.println(Arrays.toString(a));
-        lRotate(a,k);
+
+        int k=2;//rotation
+        int n=a.length;
+        k=k%n;
+
+
+        Reverse(a,0,k-1);
+        Reverse(a,k,n-1);
+        Reverse(a,0,n-1);
+        System.out.println(Arrays.toString(a));
     }
 }
+
+//Right Rotate by k      even this works but time complexity O(n*k) and space complexity O(1)
+//int[] a = {1,2,3,4,5};
+//int k = 3;
+//k = k % a.length;
+//
+//for (int i = 0; i < k; i++)
+//{
+//  int f = a[a.length-1];
+//  for (int j = a.length-1; j > 0; j--)
+//  {
+//     a[j] = a[j-1];
+//  }
+//  a[0] = f;
+//}
+
+//Left Rotate by k
+//int[] a = {1,2,3,4,5};
+//int k = 2;
+//k = k % a.length;
+//
+//for (int i = 0; i < k; i++)
+// {
+//   int f = a[0];
+//   for (int j = 0; j < a.length-1; j++)
+//   {
+//      a[j] = a[j+1];
+//   }
+//   a[a.length-1] = f;
+//}
--- Practice/src/Arrays/rightRotate.java
+++ Practice/src/Arrays/rightRotate.java
@@ -2,22 +2,28 @@ package Arrays;
 
 import java.util.Arrays;
 
-public class rightRotate
-{
-    public static void rRotate(int[] a, int k)
-    {
-        k = k % a.length;
-        for(int i=0;i<a.length;i++)
-        {
-            System.out.print(i<k? a[a.length+i-k]+" " : a[i-k]+" ");
+public class rightRotate {
+    public static void Reverse(int[] a, int start, int end) {
+        while (start < end) {
+            int temp = a[start];
+            a[start] = a[end];
+            a[end] = temp;
+            start++;
+            end--;
         }
     }
-    public static void main(String[] args)
-    {
-        int [] a={1,2,3,4,5};
-        int k=1;
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(a));
-        rRotate(a,k);
+
+        int k = 2;
+        int n = a.length;
+        k = k % n;
+        Reverse(a, 0, n - 1);
+        Reverse(a, 0, k - 1);
+        Reverse(a, k, n - 1);
+        System.out.println(Arrays.toString(a));
     }
 
 }
